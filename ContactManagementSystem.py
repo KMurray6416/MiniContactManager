fd312a9f74520833fc7a05edc7ac44950c85ee1b
KMurray6416 <kmurray6416@gmail.com>
2024-10-19 22:06:58 -0600

    still a work in progress

---
+++ ContactManagementSystem.py
@@ -0,0 +1,17 @@
+import os
+import re
+import json
+
+print(''' Welcome to the Contact Management System! 
+
+Menu:
+
+1. Add a new contact
+2. Edit an existing contact
+3. Delete a contact
+4. Search for a contact
+5. Display all contacts
+6. Export contacts to a text file
+7. Import contacts from a text file *BONUS*
+8. Quit 
+''')
